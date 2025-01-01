f2717d449f1f77ad2f8289c8df006d03901bc8e4
maheshjamadade <jamadademahesh88@gmail.com>
2025-01-01 23:37:12 +0530

    Footer edited.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,9 @@ const Footer = () => {
   return (
    <footer className='text-center bg-neutral-600 bg-opacity-35 text-white py-2'>
     <div className='flex items-center justify-center gap-4'>
-    <Link to="/">About</Link>
-    <Link to="/">Contact</Link>
+    <h5>Contact Us Using</h5>
+    <h6>Email: movieo@gmail.com</h6>
+    <h6>Mobile: 8989897766</h6>
     </div>
     <p className='text-sm'>Created By Dyanamic Coding with Mahesh</p>
    </footer>
